29d2c09fe37b76588033af57ea64ce6fbe2926d2
infinixs5lite <kelvinchinedu97@gmail.com>
2024-08-10 22:05:08 +0100

    Update omni_W6002.mk

--- omni_W6002.mk
+++ omni_W6002.mk
@@ -1,11 +1,13 @@
 #
-# Copyright (C) 2017 The Android Open Source Project
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,14 +16,20 @@
 # limitations under the License.
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from W6002 device
+$(call inherit-product, device/W6002/device.mk)
+
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-$(call inherit-product, build/target/product/embedded.mk)
-
-# Inherit from device configuration
-$(call inherit-product, device/W6002//device.mk)
-
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := W6002
 PRODUCT_NAME := omni_W6002
 PRODUCT_BRAND := Itel
