9afb983ec2b9e3f94bb449b1e0037cb7c660b7a3
infinixs5lite <kelvinchinedu97@gmail.com>
2024-08-10 22:51:02 +0100

    Update omni_W6002.mk

--- omni_W6002.mk
+++ omni_W6002.mk
@@ -19,7 +19,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from W6002 device
 $(call inherit-product, device/W6002/device.mk)
